ea5f4bbb26b8c36e20e49b9cc853a7c76283e4c1
Marsha Annon <marshaannon@gmail.com>
2019-04-09 18:13:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,4 +10,4 @@ var topic = "space"
 
 var conversation = 'Let\'s talk about space'
 
-`Hello, ${specialGuest}! Let's talk about space` // "Hello, Neil deGrasse Tyson! Let's talk about space"
+`Hello, ${specialGuest}! ${Let's talk about space}` // "Hello, Neil deGrasse Tyson! Let's talk about space"
